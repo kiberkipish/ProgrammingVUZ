51587482b830e1008c0566c246a8284c2efc07da
kiberkipish <maksimizumnikov@gmail.com>
2025-10-09 20:35:31 +0200

    Add files via upload

---
+++ Lab3.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main() {
+	long long x;
+	std::cin >> x;
+	while (x < 0 or x > 1000000000) {
+		std::cout << "Error" << std::endl;
+		std::cin >> x;
+	}
+	long long i;
+	std::cin >> i;
+	long long maska;
+	maska = ~(1 << i);
+	std::cout << (maska & x) << std::endl;
+}
